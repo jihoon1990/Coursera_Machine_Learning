61e5911d5527d5e9cb7cb947ee880d0025c0eeb4
Jihoon Kim <jihoonerd@outlook.com>
2017-04-14 23:47:16 +0900

    Assessing Performance Python Code Uploaded

---
+++ Assessing_Performance/Bias-Variance_Tradeoff/assessing_perf_func.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 14 18:12:54 2017
+
+@author: Jihoon_Kim
+"""
+
+# Assessing Performance Functions
+import pandas as pd
+
+def polynomial_dataframe(feature, degree): # feature is pandas.Series type
+    # assume that degree >= 1
+    # initialize the dataframe:
+    poly_dataframe = pd.DataFrame()
+    # and set poly_dataframe['power_1'] equal to the passed feature
+    poly_dataframe['power_1'] = feature
+    # first check if degree > 1
+    if degree > 1:
+        # then loop over the remaining degrees:
+        for power in range(2, degree+1):
+            # first we'll give the column a name:
+            name = 'power_' + str(power)
+            # assign poly_dataframe[name] to be feature^power; use apply(*)
+            poly_dataframe[name] = feature.apply(lambda x: x ** power)
+    return poly_dataframe
+
