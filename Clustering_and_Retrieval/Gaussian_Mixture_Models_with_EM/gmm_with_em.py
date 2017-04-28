7c814e1bf137f1e647bc4e41e5cc04c44833a224
Jihoon Kim <jihoon_kim@outlook.com>
2017-04-28 18:29:51 +0900

    gmm em

--- Clustering_and_Retrieval/Gaussian_Mixture_Models_with_EM/gmm_with_em.py
+++ Clustering_and_Retrieval/Gaussian_Mixture_Models_with_EM/gmm_with_em.py
@@ -12,6 +12,124 @@ Created on Fri Apr 28 15:23:02 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
-from scipy.stats import multivariate_normal
+
 from gmm_with_em_func import generate_MoG_data
+from gmm_with_em_func import compute_weights
+from gmm_with_em_func import compute_responsibilities
+from gmm_with_em_func import compute_soft_counts
+from gmm_with_em_func import compute_means
+from gmm_with_em_func import compute_covariances
+from gmm_with_em_func import EM
+from gmm_with_em_func import plot_contours
+from gmm_with_em_func import loglikelihood
+# check point
+resp = compute_responsibilities(data=np.array([[1.,2.],[-1.,-2.]]), weights=np.array([0.3, 0.7]),
+                                means=[np.array([0.,0.]), np.array([1.,1.])],
+                                covariances=[np.array([[1.5, 0.],[0.,2.5]]), np.array([[1.,1.],[1.,2.]])])
+
+if resp.shape==(2,2) and np.allclose(resp, np.array([[0.10512733, 0.89487267], [0.46468164, 0.53531836]])):
+    print('Checkpoint passed!')
+else:
+    print('Check your code again.')
+    
+# Checkpoint
+resp = compute_responsibilities(data=np.array([[1.,2.],[-1.,-2.],[0,0]]), weights=np.array([0.3, 0.7]),
+                                means=[np.array([0.,0.]), np.array([1.,1.])],
+                                covariances=[np.array([[1.5, 0.],[0.,2.5]]), np.array([[1.,1.],[1.,2.]])])
+counts = compute_soft_counts(resp)
+weights = compute_weights(counts)
+
+print(counts)
+print(weights)
+
+if np.allclose(weights, [0.27904865942515705, 0.720951340574843]):
+    print('Checkpoint passed!')
+else:
+    print('Check your code again.')
+    
+
+# check point
+data_tmp = np.array([[1.,2.],[-1.,-2.]])
+resp = compute_responsibilities(data=data_tmp, weights=np.array([0.3, 0.7]),
+                                means=[np.array([0.,0.]), np.array([1.,1.])],
+                                covariances=[np.array([[1.5, 0.],[0.,2.5]]), np.array([[1.,1.],[1.,2.]])])
+counts = compute_soft_counts(resp)
+means = compute_means(data_tmp, resp, counts)
+
+if np.allclose(means, np.array([[-0.6310085, -1.262017], [0.25140299, 0.50280599]])):
+    print('Checkpoint passed!')
+else:
+    print('Check your code again.')
+    
+# check point
+data_tmp = np.array([[1.,2.],[-1.,-2.]])
+resp = compute_responsibilities(data=data_tmp, weights=np.array([0.3, 0.7]),
+                                means=[np.array([0.,0.]), np.array([1.,1.])],
+                                covariances=[np.array([[1.5, 0.],[0.,2.5]]), np.array([[1.,1.],[1.,2.]])])
+counts = compute_soft_counts(resp)
+means = compute_means(data_tmp, resp, counts)
+covariances = compute_covariances(data_tmp, resp, counts, means)
+
+if np.allclose(covariances[0], np.array([[0.60182827, 1.20365655], [1.20365655, 2.4073131]])) and \
+    np.allclose(covariances[1], np.array([[ 0.93679654, 1.87359307], [1.87359307, 3.74718614]])):
+    print('Checkpoint passed!')
+else:
+    print('Check your code again.')
+    
+
+# Model parameters
+init_means = [
+    [5, 0], # mean of cluster 1
+    [1, 1], # mean of cluster 2
+    [0, 5]  # mean of cluster 3
+]
+init_covariances = [
+    [[.5, 0.], [0, .5]], # covariance of cluster 1
+    [[.92, .38], [.38, .91]], # covariance of cluster 2
+    [[.5, 0.], [0, .5]]  # covariance of cluster 3
+]
+init_weights = [1/4., 1/2., 1/4.]  # weights of each cluster
+
+# Generate data
+np.random.seed(4)
+data = generate_MoG_data(100, init_means, init_covariances, init_weights)
+
+plt.figure()
+d = np.vstack(data)
+plt.plot(d[:,0], d[:,1],'ko')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+np.random.seed(4)
+
 # Initialization of parameters
+chosen = np.random.choice(len(data), 3, replace=False)
+initial_means = [data[x] for x in chosen]
+initial_covs = [np.cov(data, rowvar=0)] * 3
+initial_weights = [1/3.] * 3
+
+# Run EM 
+results = EM(data, initial_means, initial_covs, initial_weights)
+
+# Parameters after initialization
+plot_contours(data, initial_means, initial_covs, 'Initial clusters')
+
+# Parameters after 12 iterations
+results = EM(data, initial_means, initial_covs, initial_weights, maxiter= 12) # YOUR CODE HERE
+plot_contours(data, results['means'], results['covs'], 'Clusters after 12 iterations')
+
+# Parameters after running EM to convergence
+results = EM(data, initial_means, initial_covs, initial_weights)
+plot_contours(data, results['means'], results['covs'], 'Final clusters')
+
+
+results = EM(data, initial_means, initial_covs, initial_weights)
+
+# YOUR CODE HERE
+loglikelihoods = results['loglik']
+
+plt.plot(range(len(loglikelihoods)), loglikelihoods, linewidth=4)
+plt.xlabel('Iteration')
+plt.ylabel('Log-likelihood')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
