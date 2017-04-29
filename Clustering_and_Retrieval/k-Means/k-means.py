7fe178acce5cf5be49ee970aafe38f48f054d971
Jihoon Kim <jihoonerd@outlook.com>
2017-04-29 17:58:12 +0900

    minor update

--- Clustering_and_Retrieval/k-Means/k-means.py
+++ Clustering_and_Retrieval/k-Means/k-means.py
@@ -171,6 +171,7 @@ visualize_document_clusters(wiki, tf_idf, centroids[2], cluster_assignment[2], 2
 k = 10
 visualize_document_clusters(wiki, tf_idf, centroids[k], cluster_assignment[k], k, map_index_to_word)
 np.bincount(cluster_assignment[10])
+
 visualize_document_clusters(wiki, tf_idf, centroids[25], cluster_assignment[25], 25, map_index_to_word, display_content=False) # turn off text for brevity
 k = 100
 visualize_document_clusters(wiki, tf_idf, centroids[k], cluster_assignment[k], k, map_index_to_word, display_content=False) # turn off text for brevity -- turn it on if you are curious ;)
