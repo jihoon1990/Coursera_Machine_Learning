306a4abdf1cd1a2b679d4f55d95c024488511e7f
Jihoon Kim <jihoon_kim@outlook.com>
2017-04-29 10:42:42 +0900

    gmm em (text) done

---
+++ Clustering_and_Retrieval/Gaussian_Mixture_Models_with_EM/gmm_with_em_text_func.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 00:51:17 2017
+
+@author: Jihoon_Kim
+"""
+
+import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse import spdiags
+from sklearn.metrics import pairwise_distances
+from copy import deepcopy
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    data = loader['data']
+    indices = loader['indices']
+    indptr = loader['indptr']
+    shape = loader['shape']
+    
+    return csr_matrix( (data, indices, indptr), shape)
+
+def diag(array):
+    n = len(array)
+    return spdiags(array, 0, n, n)
+
+def logpdf_diagonal_gaussian(x, mean, cov):
+    '''
+    Compute logpdf of a multivariate Gaussian distribution with diagonal covariance at a given point x.
+    A multivariate Gaussian distribution with a diagonal covariance is equivalent
+    to a collection of independent Gaussian random variables.
+
+    x should be a sparse matrix. The logpdf will be computed for each row of x.
+    mean and cov should be given as 1D numpy arrays
+    mean[i] : mean of i-th variable
+    cov[i] : variance of i-th variable'''
+
+    n = x.shape[0]
+    dim = x.shape[1]
+    assert(dim == len(mean) and dim == len(cov))
+
+    # multiply each i-th column of x by (1/(2*sigma_i)), where sigma_i is sqrt of variance of i-th variable.
+    scaled_x = x.dot( diag(1./(2*np.sqrt(cov))) )
+    # multiply each i-th entry of mean by (1/(2*sigma_i))
+    scaled_mean = mean/(2*np.sqrt(cov))
+
+    # sum of pairwise squared Eulidean distances gives SUM[(x_i - mean_i)^2/(2*sigma_i^2)]
+    return -np.sum(np.log(np.sqrt(2*np.pi*cov))) - pairwise_distances(scaled_x, [scaled_mean], 'euclidean').flatten()**2
+
+def log_sum_exp(x, axis):
+    '''Compute the log of a sum of exponentials'''
+    x_max = np.max(x, axis=axis)
+    if axis == 1:
+        return x_max + np.log( np.sum(np.exp(x-x_max[:,np.newaxis]), axis=1) )
+    else:
+        return x_max + np.log( np.sum(np.exp(x-x_max), axis=0) )
+    
+def EM_for_high_dimension(data, means, covs, weights, cov_smoothing=1e-5, maxiter=int(1e3), thresh=1e-4, verbose=False):
+    # cov_smoothing: specifies the default variance assigned to absent features in a cluster.
+    #                If we were to assign zero variances to absent features, we would be overconfient,
+    #                as we hastily conclude that those featurese would NEVER appear in the cluster.
+    #                We'd like to leave a little bit of possibility for absent features to show up later.
+    n = data.shape[0]
+    dim = data.shape[1]
+    mu = deepcopy(means)
+    Sigma = deepcopy(covs)
+    K = len(mu)
+    weights = np.array(weights)
+
+    ll = None
+    ll_trace = []
+
+    for i in range(maxiter):
+        # E-step: compute responsibilities
+        logresp = np.zeros((n,K))
+        for k in range(K):
+            logresp[:,k] = np.log(weights[k]) + logpdf_diagonal_gaussian(data, mu[k], Sigma[k])
+        ll_new = np.sum(log_sum_exp(logresp, axis=1))
+        if verbose:
+            print(ll_new)
+        logresp -= np.vstack(log_sum_exp(logresp, axis=1))
+        resp = np.exp(logresp)
+        counts = np.sum(resp, axis=0)
+
+        # M-step: update weights, means, covariances
+        weights = counts / np.sum(counts)
+        for k in range(K):
+            mu[k] = (diag(resp[:,k]).dot(data)).sum(axis=0)/counts[k]
+            mu[k] = mu[k].A1
+
+            Sigma[k] = diag(resp[:,k]).dot( data.multiply(data)-2*data.dot(diag(mu[k])) ).sum(axis=0) \
+                       + (mu[k]**2)*counts[k]
+            Sigma[k] = Sigma[k].A1 / counts[k] + cov_smoothing*np.ones(dim)
+
+        # check for convergence in log-likelihood
+        ll_trace.append(ll_new)
+        if ll is not None and (ll_new-ll) < thresh and ll_new > -np.inf:
+            ll = ll_new
+            break
+        else:
+            ll = ll_new
+
+    out = {'weights':weights,'means':mu,'covs':Sigma,'loglik':ll_trace,'resp':resp}
+
+    return out
+
+# Fill in the blanks
+def visualize_EM_clusters(tf_idf, means, covs, map_index_to_word):
+    print('')
+    print('==========================================================')
+
+    num_clusters = len(means)
+    for c in range(num_clusters):
+        print('Cluster {0:d}: Largest mean parameters in cluster '.format(c))
+        print('\n{0: <12}{1: <12}{2: <12}'.format('Word', 'Mean', 'Variance'))
+        
+        # The k'th element of sorted_word_ids should be the index of the word 
+        # that has the k'th-largest value in the cluster mean. Hint: Use np.argsort().
+        sorted_word_ids = np.argsort(-means[c])  # YOUR CODE HERE
+
+        for i in sorted_word_ids[:5]:
+            print('{0: <12}{1:<10.2e}{2:10.2e}'.format(map_index_to_word.index[i], 
+                                                       means[c][i],
+                                                       covs[c][i]))
+        print('\n=====================================================Quiz Question. Select all the topics that have a cluster in the model created above. [multiple choice]====')
